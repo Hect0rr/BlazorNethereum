8259aafd25d9617880044f7fd559b8d5f2af466e
hect0rr <yan.santerre@gmail.com>
2025-08-08 21:17:15 -0400

    add launcher for testing later

---
+++ BlazorNethereum.HardHatLauncher/Program.cs
@@ -0,0 +1,17 @@
+﻿// Start Blazor app
+using System.Diagnostics;
+
+Console.WriteLine("Starting Blazor app...");
+var blazorProcess = new Process
+{
+    StartInfo = new ProcessStartInfo
+    {
+        FileName = "dotnet",
+        Arguments = "run --project ../BlazorNethereum.SPA/BlazorNethereum.SPA.csproj",
+        UseShellExecute = false
+    }
+};
+blazorProcess.Start();
+
+// Wait for Blazor to exit
+await blazorProcess.WaitForExitAsync();
